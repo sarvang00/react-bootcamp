d3782ddb368af2997e223687904c0933d9b803c8
Sarvang Dave <sarvang00@gmail.com>
2021-12-14 20:41:27 +0530

    Bringing in the Navbar

---
+++ 2-appsite/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import NavBar from "./Navbar";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Sarvang</h1>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default App
