6009f59132d3e8c0bf2e78b3a0cb52bb731f2388
Sarvang Dave <sarvang00@gmail.com>
2021-12-14 19:25:10 +0530

    More reusable components

--- 1-helloworld/src/app.js
+++ 1-helloworld/src/app.js
@@ -1,11 +1,16 @@
 import React from "react";
+
 import "./style.css"
+import Button from "./Button";
 
 const App = () => {
     return(
         <div>
             <h1 style={{textAlign:"center"}}>Hello World, Again!?</h1>
-            <button className="button">Boobytrap</button>
+            <Button title="App Store"/>
+            <Button title="Play Store"/>
+            {/* <button className="button">App Store</button>
+            <button className="button">Play Store</button> */}
         </div>
     )
 }
