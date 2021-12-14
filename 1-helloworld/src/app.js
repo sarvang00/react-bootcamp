ecb24024cb8ddc8eb6cdd88bb6e7a1d5d75332c0
Sarvang Dave <sarvang00@gmail.com>
2021-12-14 19:13:28 +0530

    Splitting into individual files

---
+++ 1-helloworld/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+const App = () => {
+    return(
+        <div>
+            <h1 style={{textAlign:"center"}}>Hello World, Again!?</h1>
+            <button className="button">Boobytrap</button>
+        </div>
+    )
+}
+
+export default App
--- 1-helloworld/src/index.js
+++ 1-helloworld/src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style.css"
 
-function App() {
-    return(
-        <div>
-            <h1 style={{textAlign:"center"}}>Hello World, Again!?</h1>
-            <button className="button">Boobytrap</button>
-        </div>
-    )
-}
+import App from "./app"
 
 ReactDOM.render(<App/>, document.getElementById("root"))
