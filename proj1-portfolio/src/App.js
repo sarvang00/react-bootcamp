871042b1f172d97a248078afa69765c8fbf20a1d
Sarvang Dave <sarvang00@gmail.com>
2021-12-16 23:23:17 +0530

    Base made

---
+++ proj1-portfolio/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Wil replicate a new template here.</p>
+    </div>
+  );
+}
+
+export default App;
