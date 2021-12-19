b53a56073671f3bf48ace67b3299a1ceb4cf87ba
Sarvang Dave <sarvang00@gmail.com>
2021-12-19 21:36:09 +0530

    Updated the missed margin in Portfolio item

--- proj1-portfolio/src/PortfolioItem.js
+++ proj1-portfolio/src/PortfolioItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const PortfolioItem = ({img_src="assets/img/portfolio/submarine.png", img_alt="..."}) => (
     // Portfolio Item
-    <div className="col-md-6 col-lg-4">
+    <div className="col-md-6 col-lg-4 mb-5">
         <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal6">
             <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                 <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
