a72c1f947693d4351913e9723ec7fd9f9c5e674d
Sarvang Dave <sarvang00@gmail.com>
2021-12-19 21:14:18 +0530

    Added Masthead

--- proj1-portfolio/src/App.js
+++ proj1-portfolio/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Navbar from "./Navbar";
+import Masthead from "./Masthead"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Masthead/>
     </div>
   );
 }
